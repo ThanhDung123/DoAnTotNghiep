6393be5c2ca83da2ad91b785ac022d36f33f87b1
Your Name <you@example.com>
2021-10-13 15:52:32 +0700

    thêm chuc nang thong ke thang

--- ShopGiayTheThao/Properties/Resources.Designer.cs
+++ ShopGiayTheThao/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace ShopGiayTheThao.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap Boxplot1 {
+            get {
+                object obj = ResourceManager.GetObject("Boxplot1", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Buddy {
             get {
                 object obj = ResourceManager.GetObject("Buddy", resourceCulture);
